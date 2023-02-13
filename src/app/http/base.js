56d7a9926be68eb9b0f392c3c8b10bb0fc9a97ac
Cian <ciangomez001@gmail.com>
2023-02-13 19:48:41 -0300

    first!

---
+++ src/app/http/base.js
@@ -0,0 +1,39 @@
+import API from "./api";
+import React from 'react';
+
+
+
+export default class BASE extends React.Component {
+
+  traerTodas() {
+    API.get()
+      .then(res => {
+        const Recetas = res.data;
+        this.setState({ Recetas });
+      })
+  }
+
+  borrar(id) {
+    API.delete(id)
+      .then(res => {
+        console.log(res);
+        console.log(res.data);
+      })
+  }
+
+  subir(recipe) {
+    API.post('', recipe)
+    .then(res => {
+      console.log(res);
+      console.log(res.data);
+    })
+  }
+
+  cambiar(id, recipe) {
+    API.put(id, recipe)
+    .then(res => {
+      console.log(res);
+      console.log(res.data);
+    })
+  }
+}
